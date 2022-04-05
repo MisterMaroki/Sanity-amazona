9d779dd699dfef349753d1fd1c072c376f52b667
Omar-macbook <omar987@hotmail.co.uk>
2022-04-05 16:31:55 +0100

    sanity all products query

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import { Typography } from '@mui/material';
 import Head from 'next/head';
 import { useEffect } from 'react';
 import Layout from '../components/Layout';
+import client from '../utils/client';
 
 export default function Home() {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const products = await client.fetch();
+				const products = await client.fetch(`*[_type == "product]`);
 			} catch (err) {}
 		};
 	});
--- utils/client.js
+++ utils/client.js
@@ -1,4 +1,5 @@
 import client from '@sanity/client';
+import config from './config';
 export default client({
 	projectId: config.projectId,
 	dataset: config.dataset,
