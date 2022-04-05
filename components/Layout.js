491ca329f78e6c2297c0446bc8401585995abf51
Omar-macbook <omar987@hotmail.co.uk>
2022-04-05 15:05:47 +0100

    update header basic styles in theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,28 @@
 import { createTheme } from '@mui/material/styles';
+import { fontWeight } from '@mui/system';
 
 export default function Layout({ title, description, children }) {
-	const theme = createTheme;
+	const theme = createTheme({
+		typography: {
+			h1: {
+				fontSize: '1.6rem',
+				fontWeight: 400,
+				margin: '1rem 0',
+			},
+			h2: {
+				fontSize: '1.4rem',
+				fontWeight: 400,
+				margin: '1rem 0',
+			},
+		},
+		palette: {
+			mode: 'light',
+			primary: {
+				main: '#f0c000',
+			},
+			secondary: {
+				main: '#208080',
+			},
+		},
+	});
 }
