f2f10809c28f1aa9382bd41ad584a4aaec31bed3
Omar-macbook <omar987@hotmail.co.uk>
2022-04-05 16:11:01 +0100

    product schema fields and types entered

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -21,5 +21,45 @@ export default {
 				hotspot: true,
 			},
 		},
+		{
+			name: 'description',
+			title: 'Description',
+			type: 'string',
+		},
+		{
+			name: 'slug',
+			title: 'Slug',
+			type: 'slug',
+			options: {
+				source: 'name',
+				maxLength: 96,
+			},
+		},
+		{
+			name: 'brand',
+			title: 'Brand',
+			type: 'string',
+		},
+		{
+			name: 'category',
+			title: 'Category',
+			type: 'string',
+		},
+		{
+			name: 'rating',
+			title: 'Rating',
+			type: 'number',
+		},
+
+		{
+			name: 'numReviews',
+			title: 'NumReviews',
+			type: 'number',
+		},
+		{
+			name: 'countInStock',
+			title: 'CountInStock',
+			type: 'number',
+		},
 	],
 };
