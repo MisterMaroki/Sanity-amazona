cfffeba238375b357aabe4a52c046d2c9eccb8e1
Omar-macbook <omar987@hotmail.co.uk>
2022-04-05 14:56:59 +0100

    restart from scratch-appjs and index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-import { StoreProvider } from '../utils/Store';
-import { SnackbarProvider } from 'notistack';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const clientSideEmotionCache = createCache({ key: 'css' });
 
@@ -14,15 +11,7 @@ function MyApp({
 }) {
 	return (
 		<CacheProvider value={emotionCache}>
-			<SnackbarProvider
-				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-			>
-				<StoreProvider>
-					<PayPalScriptProvider deferLoading={true}>
-						<Component {...pageProps} />
-					</PayPalScriptProvider>
-				</StoreProvider>
-			</SnackbarProvider>
+			<Component {...pageProps} />
 		</CacheProvider>
 	);
 }
