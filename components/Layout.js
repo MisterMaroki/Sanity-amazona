bf670188d27d07fa4a96ad195799a87989acbb39
Omar-macbook <omar987@hotmail.co.uk>
2022-04-05 15:15:20 +0100

    import and return markup

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,15 @@
+import {
+	AppBar,
+	CssBaseline,
+	Link,
+	ThemeProvider,
+	Toolbar,
+	Typography,
+} from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { fontWeight } from '@mui/system';
+import Head from 'next/head';
+import NextLink from 'next/link';
 
 export default function Layout({ title, description, children }) {
 	const theme = createTheme({
@@ -25,4 +35,23 @@ export default function Layout({ title, description, children }) {
 			},
 		},
 	});
+	return (
+		<>
+			<Head>
+				<title>{title ? `${title} - Sanity Amazona` : 'Sanity Amazona'}</title>
+				{description && <meta name="description" content={description}></meta>}
+			</Head>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<AppBar position="static" />
+				<Toolbar>
+					<NextLink href="/" passHref>
+						<Link>
+							<Typography>amazona</Typography>
+						</Link>
+					</NextLink>
+				</Toolbar>
+			</ThemeProvider>
+		</>
+	);
 }
